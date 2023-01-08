16ca71321474d9b556e857e59df358e067249f59
elody-ramirez <elody.ramirez@gmail.com>
2023-01-08 17:53:58 -0500

    Added space and time complexity and another solution with O(1) space

--- py/125_valid_palindrome.py
+++ py/125_valid_palindrome.py
@@ -26,15 +26,43 @@
 # 1 <= s.length <= 2 * 105
 # s consists only of printable ASCII characters.
 
+# O(N) Time and O(1) Space solution
 class Solution(object):
     def isPalindrome(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        newStr = ""
+        l, r = 0, len(s) - 1
 
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[::-1]
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+    
+    def alphaNum(self, c):
+        """
+        :type c: str
+        :rtype: bool
+        """
+        return (ord('A') <= ord(c) <= ord('Z')) or (ord('a') <= ord(c) <= ord('z')) or (ord('0') <= ord(c) <= ord('9'))
+                
+# O(N) Space and Time solution because i use a new string
+# class Solution(object):
+#     def isPalindrome(self, s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+#         newStr = ""
+
+#         for c in s:
+#             if c.isalnum():
+#                 newStr += c.lower()
+#         return newStr == newStr[::-1]
