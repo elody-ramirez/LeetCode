11fe9fed81bbb1316cf812a0df339c50a1f234e5
elody-ramirez <elody.ramirez@gmail.com>
2023-01-08 23:57:53 -0500

    updated the last way to make a stack using lifoqueue

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -8,3 +8,65 @@
 # Out of these three, the easiest and the most popular way for
 # implementing a stack in Python is list. Let’s see the implementation
 # of a stack in Python using lists.
+
+# Stack implementation using 
+# LIST
+
+from queue import LifoQueue
+stack = []
+
+# append() function to push
+# element in the stack
+stack.append('a')
+stack.append('b')
+# pop() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from stack:')
+print(stack.pop())
+
+########################################################################
+
+# Stack implementation using
+# collections.deque
+
+from collections import deque
+ 
+stack = deque()
+
+# append() function to push
+# element in the stack
+stack.append('a')
+stack.append('b')
+# pop() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from stack:')
+print(stack.pop())
+
+########################################################################
+
+# Python program to
+# demonstrate stack implementation
+# using queue module
+
+# Initializing a stack
+stack = LifoQueue(maxsize=3)
+
+# qsize() show the number of elements
+# in the stack
+print(stack.qsize())
+
+# put() function to push
+# element in the stack
+stack.put('a')
+stack.put('b')
+
+print("Full: ", stack.full())
+print("Size: ", stack.qsize())
+
+# get() function to pop
+# element from stack in
+# LIFO order
+print('\nElements popped from the stack')
+print(stack.get())
