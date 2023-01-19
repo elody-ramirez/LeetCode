1a2676db39347206007f3a567dae471728f0d7dc
elody-ramirez <elody.ramirez@gmail.com>
2023-01-19 10:47:38 -0500

    added the prompt for encode and decode strings

---
+++ py/Medium/271_encode_and_decode_strings.py
@@ -0,0 +1,40 @@
+# Design an algorithm to encode a list of strings to a string.
+# The encoded string is then sent over the network and is decoded
+# back to the original list of strings.
+
+# Please implement encode and decode
+
+# Wechat reply 【Google】 get the latest requent Interview questions. (wechat id : jiuzhang1104)
+
+# Example1
+
+# Input: ["lint","code","love","you"]
+# Output: ["lint","code","love","you"]
+# Explanation:
+# One possible encode method is: "lint:;code:;love:;you"
+
+# Example2
+
+# Input: ["we", "say", ":", "yes"]
+# Output: ["we", "say", ":", "yes"]
+# Explanation:
+# One possible encode method is: "we:;say:;:::;yes"
+
+class Solution:
+    """
+    Solution
+    """
+
+    def encode(self, strs):
+        # write your code here
+        """
+        @param: strs: a list of strings
+        @return: encodes a list of strings to a single string.
+        """
+
+    def decode(self, str):
+        # write your code here
+        """
+        @param: str: A string
+        @return: dcodes a single string to a list of strings
+        """
