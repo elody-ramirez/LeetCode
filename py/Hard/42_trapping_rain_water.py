135b5f70a3813c8dc0570bf9493542d5c30317e3
elody-ramirez <elody.ramirez@gmail.com>
2023-01-19 14:50:17 -0500

    Completed question

--- py/Hard/42_trapping_rain_water.py
+++ py/Hard/42_trapping_rain_water.py
@@ -25,6 +25,7 @@
 # 1 <= n <= 2 * 104
 # 0 <= height[i] <= 105
 
+# O(N) Time 0(1) Space Complexity not counting result
 class Solution(object):
     """Solution"""
     def trap(self, height):
@@ -32,3 +33,22 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        if not height:
+            return 0
+        l, r = 0, len(height) - 1
+        left_max, right_max = height[l], height[r]
+        res = 0
+
+        while l < r:
+            if left_max < right_max:
+                l += 1
+                left_max = max(left_max, height[l])
+                res += left_max - height[l]
+            else:
+                r -= 1
+                right_max = max(right_max, height[r])
+                res += right_max - height[r]
+        return res
+
+input1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+print(Solution().trap(input1))                      # Output: 6
