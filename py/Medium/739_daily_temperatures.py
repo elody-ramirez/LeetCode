776fb626346297ed6e1e9f847cc161e796ba6fbb
elody-ramirez <elody.ramirez@gmail.com>
2023-01-19 22:31:22 -0500

    Solved problem

--- py/Medium/739_daily_temperatures.py
+++ py/Medium/739_daily_temperatures.py
@@ -32,4 +32,12 @@ class Solution(object):
         :type temperatures: List[int]
         :rtype: List[int]
         """
-        
+        res = [0] * len(temperatures)
+        stack = [] # pair: [temp, index]
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stack_temp, stack_idx = stack.pop()
+                res[stack_idx] = (i - stack_idx)
+            stack.append([t, i])
+        return res
