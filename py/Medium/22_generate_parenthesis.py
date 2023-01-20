7248ff4037f623c0f0abf56d0066d698fd521a86
elody-ramirez <elody.ramirez@gmail.com>
2023-01-19 22:00:49 -0500

    Solution for generate parenthesis

--- py/Medium/22_generate_parenthesis.py
+++ py/Medium/22_generate_parenthesis.py
@@ -23,3 +23,30 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        # only add open parenthesis if open < n
+        # only add a closing parenthesis if closed < open
+        # valid IF open == closed == n
+
+        stack = []
+        res = []
+
+        def back_track(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                back_track(openN + 1, closedN)
+                stack.pop()
+
+            if closedN < openN:
+                stack.append(")")
+                back_track(openN, closedN + 1)
+                stack.pop()
+
+        back_track(0,0)
+        return res
+
+print(Solution().generate_parenthesis(3))
+# Output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
